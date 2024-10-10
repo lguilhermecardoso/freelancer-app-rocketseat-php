e65ef24db41ba3c055413b56ac9ece52eb06c207
L Guilherme Cardoso <luis.grosa@totvspartners.com.br>
2024-10-10 12:58:59 -0300

    feat: create model users and proposals

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+
+    protected static ?string $password;
+
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'rating' => fake()->numberBetween(0, 5),
+            'avatar' => 'https://avatar.iran.liara.run/public'
+        ];
+    }
+}
